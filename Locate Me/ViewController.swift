09eacb69361e0bd08c24535109d457aa320b8f1c
Fabian Vergara <vergarf@gmail.com>
2016-06-14 22:55:22 -0400

    Updated tags correctly. There is a bug for latitude and altitude...

--- Locate Me/ViewController.swift
+++ Locate Me/ViewController.swift
@@ -64,9 +64,12 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func setLabels(locations: [CLLocation]){
         let userLocation : CLLocation = locations[0]
         
-        latLabel.text = "Latitude : " + String(userLocation.coordinate.latitude)
-        lonLabel.text = "Longitude: " + String(userLocation.coordinate.longitude)
-
+        latLabel.text    = "Latitude : " + String(userLocation.coordinate.latitude)
+        lonLabel.text    = "Longitude: " + String(userLocation.coordinate.longitude)
+        latLabel.text    = "Latitude : " + String(userLocation.altitude)
+        courseLabel.text = "Course : " + String(userLocation.course)
+        speedLabel.text  = "Speed : " + String(userLocation.speed)
+ 
         
     }
 
